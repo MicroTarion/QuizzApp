7a217b92ca996a3ea8ed0a1af122a80ec6b1fe33
unknown <cecile.auger.pro@gmail.com>
2023-12-08 14:06:55 +0100

    push without comment on Quiz.jsx

--- src/Components/quiz/Quiz.jsx
+++ src/Components/quiz/Quiz.jsx
@@ -1,28 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import "./Quiz.css"
 
-
-// const Quiz = () =>{
-//     return (
-//        <>
-//             <div className="container">
-//                 <h1>Quiz App</h1> 
-//                 <hr></hr> 
-//                 <h2>notre question</h2>
-//                 <ul>
-//                     <li>reponse1</li>
-//                     <li>reponse1</li>
-//                     <li>reponse1</li>
-//                     <li>reponse1</li>
-//                 </ul>
-//                 <button>Next</button>
-//                 <div className="numb">1/10 questions</div>
-//             </div>
-//        </>
-//     )
-// }
-// export default Quiz
-
 const Quiz = () => {
     return (
         <>
