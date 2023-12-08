4bbdb0224647078f74e6d44f8aefeef771b942e1
Lakym972 <132657810+Lakym972@users.noreply.github.com>
2023-12-08 15:38:09 +0100

    gestion question/reponses

--- src/Components/quiz/Quiz.jsx
+++ src/Components/quiz/Quiz.jsx
@@ -3,6 +3,11 @@ import { useEffect, useState } from "react";
 
   const Quiz = ()=>{
   const [data, setData] = useState([])
+  const [index, setIndex] = useState(0)
+  const [correctAnswer, setCorrectAnswer] = useState("")
+  const [answers, setAnswers] = useState([])
+  const [userAnswer, setUserAnswer] = useState("")
+
 
   useEffect(() => {
 
@@ -15,36 +20,51 @@ import { useEffect, useState } from "react";
         const data = await response.json();
     
         setData(data)
+
+        setCorrectAnswer(data.results[0].correct_answer)
+        setAnswers(data.results[0].incorrect_answers.concat([data.results[0].correct_answer]).sort(() => Math.random() - 0.5))
+
       };
 
       quizzGeneration()
       
     },[]);
+
+
+
+
   return(
-    <>
-    <div className="container">
-                <h1>Quiz App</h1> 
-                <hr></hr> 
-                <h2>notre question</h2>
-                <ul>
-                    <li>reponse1</li>
-                    <li>reponse1</li>
-                    <li>reponse1</li>
-                    <li>reponse1</li>
-                </ul>
-                
-                <div className="numb">1/10 questions</div>
-            </div>
-        <div>
-            <form action="">
-            <label htmlFor="movieSearch"></label>
-            <input
-                name="movieSearch"
-                type="submit"
-                value="Quizz Generation"
-            />
-            </form>
-        </div>       
+    <> {data?.results?.length > 0 ?  <div className="container">
+        
+    <h1>Quiz App</h1> 
+    <hr></hr> 
+<h2 dangerouslySetInnerHTML={{__html:data?.results && data?.results[0]?.question}}></h2>  
+    <ul>
+       {answers.map((el) => {
+        return (
+          <li style={ userAnswer !== "" && userAnswer === el && userAnswer === correctAnswer 
+          ? {backgroundColor: "green"} 
+          : 
+              userAnswer !== "" && userAnswer === el && userAnswer !== correctAnswer
+              ? {backgroundColor:"red"} 
+              : null} 
+              
+            key={el} onClick={() => {
+
+              if(userAnswer === "") {
+                console.log(el === correctAnswer);
+                setUserAnswer(el)
+              }
+
+
+          }}>{el}</li>
+        )
+       })}
+    </ul>
+    <button>Next</button>
+    <div className="numb">1/10 questions</div>
+</div> : <>Loading</>   }
+    
     </>    
     )
 };
